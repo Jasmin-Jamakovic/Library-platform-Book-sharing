b638faa8e5853b8d3f42c53a7ce40b382cb0de3f
Jasmin-Jamakovic <jamakovicjasminl@gmail.com>
2025-09-22 21:57:36 +0200

    Added database,and schemas

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from fastapi import FastAPI
+from http.client import HTTPException
+
+from fastapi import FastAPI,Depends
 from sqlalchemy.orm import Session
 import models
 from pydantic import BaseModel
+from database import get_db
 
 
 
 app=FastAPI()
 
+from models import User
+class UserCreate(BaseModel):
+    name:str
+    email:str
+
+@app.post("/users/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    # Check if the email is already taken
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(name=user.name, email=user.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+
+
 @app.get("/")
 def root():
     return {"Message":"Poruka"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int,db: Session=Depends((get_db()))):
+    user=db.query(User).filter(User.id==user_id).first()
+    if not user:
+        raise HTTPException(status_code=404,detail="User not found")
+    return user
--- models.py
+++ models.py
@@ -22,11 +22,12 @@ class Book(Base):
     available=Column(Boolean,default=True)
     created_at =Column(DateTime,default=datetime.datetime.utcnow)
 
+class User(Base):
+    __tablename__="users"
+    id=Column(Integer,primary_key=True,index=True)
+    name=Column(String,index=True)
+    email=Column(String,unique=True,index=True)
 
 
-def get_db():
-    db=SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+Base.metadata.create_all(bind=engine)
+
